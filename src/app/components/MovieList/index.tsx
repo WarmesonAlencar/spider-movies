0a55d718ec24291763e30daecab7b8e54b05857e
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-16 13:59:16 -0300

    inserindo url e chave da API

--- src/app/components/MovieList/index.tsx
+++ src/app/components/MovieList/index.tsx
@@ -1,5 +1,21 @@
+import axios from 'axios';
+import './index.scss';
+
 export default function MovieList(){
+    const[movies, setMovies] = useState([]);
+
+    const getMovies = () => {
+        axios({
+            method: 'get',
+            url:'https://api.themoviedb.org/3/discover/movie',
+            params: {
+                api_key:'b224536c0803f50498a7118ee808a91e'
+            }
+        })
+    }
     return (
-        
+        <ul className="movie-list">
+            <li></li>
+        </ul>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar />
+        <NavBar/>
         {children}
       </body>
     </html>
