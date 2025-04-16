efb8260039d70b39bc856069fe1f528a196ff7f0
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-16 14:10:51 -0300

    chamando função get movies

--- src/app/components/MovieList/index.tsx
+++ src/app/components/MovieList/index.tsx
@@ -1,5 +1,8 @@
-import axios from 'axios';
+'use client';
+
+import { useEffect, useState } from 'react';
 import './index.scss';
+import axios from 'axios';
 
 export default function MovieList(){
     const[movies, setMovies] = useState([]);
@@ -9,10 +12,16 @@ export default function MovieList(){
             method: 'get',
             url:'https://api.themoviedb.org/3/discover/movie',
             params: {
-                api_key:'b224536c0803f50498a7118ee808a91e'
+                api_key:'b224536c0803f50498a7118ee808a91e',
+                language: 'pt-BR'
             }
+        }).then(response => {
+            console.log(response);
         })
     }
+
+    getMovies();
+
     return (
         <ul className="movie-list">
             <li></li>
